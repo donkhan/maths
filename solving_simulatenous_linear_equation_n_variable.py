c8fc35737adaa4af28c4872461609d4068dae5a2
panoptic-qs-kamil <kamil@panoptic-qs.com>
2023-03-22 21:58:56 +0530

    removed dummy entries

--- solving_simulatenous_linear_equation_n_variable.py
+++ solving_simulatenous_linear_equation_n_variable.py
@@ -39,8 +39,8 @@ def solve():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(upper_diagonalize([[1, 2, 1], [3, 4, 0], [5, 6, 2]], [4, 7, 13], 0))
-    print(upper_diagonalize([[2, 3], [3, -4]], [13, -6], 0))
-    print(upper_diagonalize([[2, 3, 4], [3, -4, 2], [-1, -2, 3]], [20, 1, 4], 0))
+    #print(upper_diagonalize([[1, 2, 1], [3, 4, 0], [5, 6, 2]], [4, 7, 13], 0))
+    #print(upper_diagonalize([[2, 3], [3, -4]], [13, -6], 0))
+    #print(upper_diagonalize([[2, 3, 4], [3, -4, 2], [-1, -2, 3]], [20, 1, 4], 0))
     solve()
 
